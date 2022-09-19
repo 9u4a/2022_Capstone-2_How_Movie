df6b2b3236c448d2899f6cd34217f1539e5f83d3
UBamtol <yij0325@gmail.com>
2022-09-20 02:08:13 +0900

    [ Web ] Fix BacgroundMovie CSS

--- HowMovie_Web/pages/BackgroundMovie.tsx
+++ HowMovie_Web/pages/BackgroundMovie.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 const BackgroundMovie: NextPage = () => {
   return (
     <>
-      <div className="relative w-full pb-[56%] border">
+      <div className="relative w-full pb-[56%]">
         <iframe
-          className="w-full h-full absolute"
+          className="w-full h-full absolute bg-gradient-to-b from-gray-900"
           src="https://www.youtube.com/embed/g4qQXXtaMqc?autoplay=1&mute=1&fs=0&modestbranding=1&disablekb=1&controls=0&iv_load_policy=3&loop=1"
           title="YouTube video player"
           allow="accelerometer; autoplay; muted; controls=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         ></iframe>
+        <div className="absolute w-full pb-[56%] bg-gradient-to-b from-black via-transparent to-black z-10 opacity-60"></div>
       </div>
     </>
   );
