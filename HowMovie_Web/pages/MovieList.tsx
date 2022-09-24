226c01420b03d4de47ec3e820a0725952afa2412
UBamtol <yij0325@gmail.com>
2022-09-24 23:55:38 +0900

    [ Web ] Fix MovieList Component

--- HowMovie_Web/pages/MovieList.tsx
+++ HowMovie_Web/pages/MovieList.tsx
@@ -2,51 +2,106 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import React from 'react';
 
-const MovieList: NextPage = () => {
+export type propsType = {
+  type: string;
+};
+
+const MovieList = ({ type }: propsType) => {
+  console.log(type);
   return (
     <div className="relative z-20">
-      <h1 className="px-[30px] font-semibold drop-shadow-lg">Top 10</h1>
-      <div className="px-[30px] flex w-full snap-x snap-mandatory overflow-x-scroll z-20 bg-gradient-to-b from-black to-[#141414] space-x-5">
-        <div className="snap-center shrink-0">
+      {/* <h1 className="px-[30px] font-semibold drop-shadow-lg">
+        {type === 'top' ? 'Top 10' : '최신 영화'}
+      </h1> */}
+      {type === 'top' ? (
+        <h1 className="px-[30px] font-semibold drop-shadow">Top 10</h1>
+      ) : (
+        <h2 className="px-[30px] font-semibold drop-shadow">최신 영화</h2>
+      )}
+      <div className="mb-[-50px] px-[30px] flex w-full snap-x snap-mandatory overflow-x-scroll z-20 bg-gradient-to-b from-[trasparent] via-black/80 to-[#141414]/80 space-x-5 scrollbar-hide">
+        <div className="relative snap-center shrink-0">
           <Image
             src="/asset/image/image 2.png"
-            width="190"
-            height="270"
+            width="152"
+            height="216"
             alt="이미지"
+            className="relative"
           />
-          <h1 className="relative z-30 ml-[10px] top-[-280px]">1</h1>
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
         </div>
-        <div className="snap-center shrink-0">
+        <div className="relative snap-center shrink-0">
           <Image
             src="/asset/image/image 2.png"
-            width="190"
-            height="270"
+            width="152"
+            height="216"
             alt="이미지"
+            className="relative"
           />
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
         </div>
-        <div className="snap-center shrink-0">
+        <div className="relative snap-center shrink-0">
           <Image
             src="/asset/image/image 2.png"
-            width="190"
-            height="270"
+            width="152"
+            height="216"
             alt="이미지"
+            className="relative"
           />
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
         </div>
-        <div className="snap-center shrink-0">
+        <div className="relative snap-center shrink-0">
           <Image
             src="/asset/image/image 2.png"
-            width="190"
-            height="270"
+            width="152"
+            height="216"
             alt="이미지"
+            className="relative"
           />
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
         </div>
-        <div className="snap-center shrink-0">
+        <div className="relative snap-center shrink-0">
           <Image
             src="/asset/image/image 2.png"
-            width="190"
-            height="270"
+            width="152"
+            height="216"
             alt="이미지"
+            className="relative"
           />
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
+        </div>
+        <div className="relative snap-center shrink-0">
+          <Image
+            src="/asset/image/image 2.png"
+            width="152"
+            height="216"
+            alt="이미지"
+            className="relative"
+          />
+          {type === 'top' && (
+            <h1 className="relative z-30 ml-[10px] top-[-230px] italic font-semibold drop-shadow">
+              1
+            </h1>
+          )}
         </div>
       </div>
     </div>
--- HowMovie_Web/pages/index.tsx
+++ HowMovie_Web/pages/index.tsx
@@ -3,13 +3,15 @@ import Head from 'next/head';
 import Image from 'next/image';
 import BackgroundMovie from './BackgroundMovie';
 import MovieList from './MovieList';
+// import { propsType } from './index';
 
 const Home: NextPage = () => {
   return (
     <div className="relative">
       <BackgroundMovie />
-      <div className="absolute top-[60%] w-full h-full">
-        <MovieList />
+      <div className="absolute top-[75%] w-full ">
+        <MovieList type="top" />
+        <MovieList type="new" />
         <div>HowMovie</div>
       </div>
     </div>
