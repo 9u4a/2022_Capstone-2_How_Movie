0bb4a0aa7445adb250a4039d8bdc8c51f19ebc0f
UBamtol <yij0325@gmail.com>
2022-09-30 19:53:33 +0900

    [ Web ] Fix Button CSS in MainPosters

--- HowMovie_Web/pages/MainPosters.tsx
+++ HowMovie_Web/pages/MainPosters.tsx
@@ -5,10 +5,11 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 type MyPostersProps = {};
 
 function MainPosters() {
+  const onClick = () => {};
   return (
     <>
       <div className="relative w-full group h-[400px] md:h-[600px] border flex justify-center items-center duration-700 space-x-5">
-        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 top-[-20px]">
+        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 shrink-0">
           <Image
             src="/asset/image/image 2.png"
             sizes="100%"
@@ -20,7 +21,7 @@ function MainPosters() {
             className="border relative"
           />
         </div>
-        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700">
+        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 shrink-0">
           <Image
             src="/asset/image/image 12.png"
             sizes="100%"
@@ -32,7 +33,7 @@ function MainPosters() {
             className="border relative"
           />
         </div>
-        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 top-[-20px]">
+        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 shrink-0">
           <Image
             src="/asset/image/image 15.png"
             sizes="100%"
@@ -44,11 +45,35 @@ function MainPosters() {
             className="border relative"
           />
         </div>
-        <div className="absolute invisible group-hover:visible flex justify-between items-center w-full">
+        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 shrink-0">
+          <Image
+            src="/asset/image/image 15.png"
+            sizes="100%"
+            width={100}
+            height={100}
+            alt="이미지"
+            layout="fill"
+            objectFit="contain"
+            className="border relative"
+          />
+        </div>
+        <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] duration-700 shrink-0">
+          <Image
+            src="/asset/image/image 15.png"
+            sizes="100%"
+            width={100}
+            height={100}
+            alt="이미지"
+            layout="fill"
+            objectFit="contain"
+            className="border relative"
+          />
+        </div>
+        <div className="absolute invisible group-hover:visible flex justify-between items-center w-full left-[-20px]">
           <div className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer">
             <ChevronLeftIcon className="w-8 h-8" />
           </div>
-          <div className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer">
+          <div className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer ">
             <ChevronRightIcon className="w-8 h-8" />
           </div>
         </div>
