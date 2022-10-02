9edaeb947131beb4075564d911aa6b687c3c2bdc
UBamtol <yij0325@gmail.com>
2022-10-02 23:57:34 +0900

    [ Web ] Fix MovieList Component

--- HowMovie_Web/pages/MainPosters.tsx
+++ HowMovie_Web/pages/MainPosters.tsx
@@ -3,12 +3,38 @@ import React, { useRef, useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 type MyPostersProps = {};
-// type StyleType = {
-//   style: string;
-//   setStyle: string;
-// }
+type moveWidthType = {
+  [key: number]: string;
+};
 
 function MainPosters() {
+  const IMG_WIDTH = 320;
+  const moveWidth = [
+    [
+      'translate-x-[0px]',
+      'translate-x-[-220px]',
+      'translate-x-[-440px]',
+      'translate-x-[-660px]',
+      'translate-x-[-880px]',
+      'translate-x-[-1100px]',
+    ],
+    [
+      'translate-x-[0px]',
+      'translate-x-[-290px]',
+      'translate-x-[-580px]',
+      'translate-x-[-870px]',
+      'translate-x-[-1160px]',
+      'translate-x-[-1450px]',
+    ],
+    [
+      'translate-x-[0px]',
+      'translate-x-[-320px]',
+      'translate-x-[-640px]',
+      'translate-x-[-960px]',
+      'translate-x-[-1280px]',
+      'translate-x-[-1640px]',
+    ],
+  ];
   const images = [
     {
       src: '/asset/image/image 2.png',
@@ -41,51 +67,71 @@ function MainPosters() {
       src: '/asset/image/image 15.png',
     },
   ];
-  const IMG_WIDTH = 300;
   const [move, setMove] = useState(0);
-  const [style, setStyle] = useState(`translate-x-${move}`);
   const moveRight = () => {
-    if (move >= -(images.length * IMG_WIDTH) + IMG_WIDTH * 3) {
-      setMove(move - IMG_WIDTH);
-    }
+    setMove(move + 1);
+    console.log('moveRight');
   };
   const moveLeft = () => {
-    if (move >= -IMG_WIDTH) {
-      setMove(move + IMG_WIDTH);
+    if (move != 0) {
+      setMove(move - 1);
+      console.log('moveLeft');
     }
   };
+  // const moveRight = () => {
+  //   if (move >= -(images.length * IMG_WIDTH) + IMG_WIDTH * 3) {
+  //     setMove(move - IMG_WIDTH);
+  //     console.log('moveRight');
+  //   }
+  // };
+  // const moveLeft = () => {
+  //   if (move <= -IMG_WIDTH) {
+  //     setMove(move + IMG_WIDTH);
+  //     console.log('moveLeft');
+  //   }
+  // };
+  // const moveWidth: moveWidthType = { 0: `translate-x-[${move}px]` };
+  console.log(moveWidth);
 
-  // const imgSize = useRef(images.current.length);
   return (
-    <>
-      <div className="relative w-full group h-[400px] md:h-[600px] border flex justify-center items-center duration-700 space-x-5 overflow-hidden">
-        {images.map((e) => {
-          return (
-            <>
-              <div className="relative w-[200px] h-[284px] md:w-[270px] md:h-[384px] lg:w-[300px] lg:h-[426px] duration-700 shrink-0">
-                <Image
-                  src={e.src}
-                  sizes="100%"
-                  width={100}
-                  height={100}
-                  alt="이미지"
-                  layout="fill"
-                  objectFit="contain"
-                />
+    <div className="border overflow-hidden group">
+      <div
+        className={`${moveWidth[0][move]} md:${moveWidth[1][move]} lg:${moveWidth[2][move]} w-full h-[400px] md:h-[600px] border flex items-center duration-700 space-x-5`}
+      >
+        <>
+          {images.map((e, i) => {
+            return (
+              <div key={i}>
+                <div className="relative flex w-[200px] h-[284px] md:w-[270px] md:h-[384px] lg:w-[300px] lg:h-[426px] duration-700 shrink-0">
+                  <Image
+                    src={e.src}
+                    sizes="100%"
+                    alt="이미지"
+                    layout="fill"
+                    objectFit="contain"
+                    priority
+                  />
+                </div>
               </div>
-            </>
-          );
-        })}
-        <div className="absolute invisible group-hover:visible flex justify-between items-center w-full left-[-20px]">
-          <div className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer">
-            <ChevronLeftIcon className="w-8 h-8" onClick={moveLeft} />
-          </div>
-          <div className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer ">
-            <ChevronRightIcon className="w-8 h-8" onClick={moveRight} />
-          </div>
+            );
+          })}
+        </>
+      </div>
+      <div className="top-[15%] md:top-[20%] absolute invisible group-hover:visible flex justify-between items-center w-full">
+        <div
+          className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer"
+          onClick={moveLeft}
+        >
+          <ChevronLeftIcon className="w-8 h-8" />
+        </div>
+        <div
+          className="w-[50px] h-[100px] flex justify-center items-center bg-slate-50/50 hover:cursor-pointer"
+          onClick={moveRight}
+        >
+          <ChevronRightIcon className="w-8 h-8" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
