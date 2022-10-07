0df341676fda91d76158a5c3196ae4699082981d
UBamtol <yij0325@gmail.com>
2022-10-08 03:10:41 +0900

    [ Web ] Add SearchBar

--- HowMovie_Web/components/layout/header.tsx
+++ HowMovie_Web/components/layout/header.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import Login from '../../pages/Login';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Header = () => {
   const headerType = [
     { id: 0, title: '홈', type: 'home' },
-    { id: 1, title: '최신', type: 'latest' },
-    { id: 2, title: '인기', type: 'popular' },
+    { id: 1, title: '장르별', type: 'genre' },
   ];
+  const testSearch = ['공조', '공조2', '탑건', '탑건 메버릭'];
+
+  const [isToggle, setIsToggle] = useState(false);
+  const onClick = () => {
+    setIsToggle(!isToggle);
+  };
 
   return (
     <>
@@ -22,22 +28,28 @@ const Header = () => {
               width={25}
               height={25}
             />
-            <h4 className="ml-[5px] mr-[70px] font-semibold">무비어때</h4>
-            <div className="flex space-x-[20px] text-gray-500 ">
-              <div className="text-white">홈</div>
-              <div>최신</div>
-              <div>인기</div>
-            </div>
+            <h4 className="ml-[5px] font-semibold">무비어때</h4>
           </a>
         </Link>
         <div className="flex items-center">
-          <MagnifyingGlassIcon className="w-[20px] h-[20px] mr-[20px]" />
+          <MagnifyingGlassIcon
+            className={`w-[20px] h-[20px] mr-[20px] hover:cursor-pointer z-10 ${
+              isToggle ? 'text-black' : 'text-white'
+            } duration-1000`}
+            onClick={() => onClick()}
+          />
+          <input
+            className={`absolute ${
+              isToggle ? 'w-[200px] px-[10px]' : 'w-0'
+            } h-[30px]  text-black right-[110px] rounded-lg bg-slate-300 duration-500`}
+            placeholder="영화를 검색하세요..."
+            id="search"
+          ></input>
           <Link href="/Login">
             <a>로그인</a>
           </Link>
         </div>
       </div>
-      {/* {isLoginOpen && <Login setIsLoginOpen={setIsLoginOpen} />} */}
     </>
   );
 };
