84dc393ead631e8794985cadec9b60805cda2197
Tasty-Programmer <kimhunsup159@gmail.com>
2022-10-15 00:17:23 +0900

    [ Login ] Feat JWT

---
+++ oauth2/src/main/java/comoutsource/oauth2/config/jwt/JwtAuthFilter.java
@@ -0,0 +1,46 @@
+package comoutsource.oauth2.config.jwt;
+
+import comoutsource.oauth2.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+public class JwtAuthFilter extends GenericFilterBean {
+    private final TokenService tokenService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = ((HttpServletRequest)request).getHeader("Auth");
+
+        if (token != null && tokenService.verifyToken(token)) {
+            String email = tokenService.getUid(token);
+
+            UserDto userDto = UserDto.builder()
+                    .email(email)
+                    .name("이름")
+                    .picture("프로필이미지").build();
+
+            Authentication auth = getAuthentication(userDto);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public Authentication getAuthentication(UserDto member) {
+        return new UsernamePasswordAuthenticationToken(member, "",
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
