2295caae8dd9fb76df9dbc1d3b7e23a0e096bb08
UBamtol <yij0325@gmail.com>
2022-10-14 11:05:50 +0900

    [ Web ] Fix BackgroundMovie Logic

--- HowMovie_Web/components/BackgroundMovie.tsx
+++ HowMovie_Web/components/BackgroundMovie.tsx
@@ -12,9 +12,9 @@ const BackgroundMovie = ({ currID }: Props) => {
   const baseUrl = 'https://image.tmdb.org/t/p/original';
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error>();
-  const [moviePath, setMoviePath] = useState<any>();
+  const [moviePath, setMoviePath] = useState<any>('');
   const [backDrop, setBackDrop] = useState<any>('');
-  console.log(currID);
+
   useEffect(() => {
     const fetchBackgroundPath = async () => {
       try {
@@ -22,7 +22,6 @@ const BackgroundMovie = ({ currID }: Props) => {
         const res = await axios.get(
           `http://localhost:8000/moviedetail?movie_id=${currID}`
         );
-        // console.log(res.data.result[0].detail[0].backdrop_path);
         setBackDrop(res.data.result[0].detail[0].backdrop_path);
         setMoviePath(res.data.result[0].detail[0].video[0].video);
       } catch (err) {
@@ -32,15 +31,17 @@ const BackgroundMovie = ({ currID }: Props) => {
       }
       setLoading(false);
     };
+
     fetchBackgroundPath();
   }, [currID]);
 
   loading && <div>로딩중</div>;
   error && <div>에러 발생</div>;
   !moviePath && null;
+
   return (
     <>
-      {moviePath !== undefined ? (
+      {moviePath !== '' ? (
         <div className="relative w-full pb-[56.3%] max-h-[350px] bg-black rounded-t-xl">
           <iframe
             className="w-full h-full absolute from-black mt-[10px]"
@@ -49,7 +50,7 @@ const BackgroundMovie = ({ currID }: Props) => {
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
         </div>
-      ) : (
+      ) : backDrop !== '' ? (
         <div className="relative w-full pb-[56.3%] max-h-[350px] bg-black rounded-t-xl">
           <Image
             src={baseUrl + backDrop}
@@ -58,15 +59,7 @@ const BackgroundMovie = ({ currID }: Props) => {
             className="rounded-t-xl"
           />
         </div>
-      )}
-      {/* <div className="relative w-full pb-[55%] max-h-[350px] bg-black rounded-t-xl">
-        <iframe
-          className="w-full h-full absolute from-black mt-[10px]"
-          src={`https://www.youtube.com/embed/${moviePath}?autoplay=1&fs=0&modestbranding=1&disablekb=1&controls=0`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-      </div> */}
+      ) : null}
     </>
   );
 };
