86173c96eb11c6ead76e70c4ca42f86bf8e0f0da
UBamtol <yij0325@gmail.com>
2022-10-22 23:53:38 +0900

    [ Web ] Fix Carousel Logic

--- HowMovie_Web/components/MainPosters.tsx
+++ HowMovie_Web/components/MainPosters.tsx
@@ -15,45 +15,68 @@ function MainPosters({ listType }: PropsType) {
   }
 
   const [innerWidth, setInnerWidth] = useState(initialWidth);
+  const [transition, setTransition] = useState('');
+  const setSlides = () => {
+    const addedFront = [];
+    const addedLast = [];
+    var index = 0;
+    while (index < 3) {
+      addedFront.push(listType[index % listType.length]);
+      addedLast.push(listType[listType.length - 3 + (index % listType.length)]);
+      index++;
+    }
+    return [...addedLast, ...listType, ...addedFront];
+  };
+  const carousel = listType && setSlides();
   useEffect(() => {
     const resizeListener = () => {
       setInnerWidth(window.innerWidth);
     };
     window.addEventListener('resize', resizeListener);
   }, [innerWidth]);
-
+  const replaceCarousel = (a: number, b: number) => {
+    setTimeout(() => {
+      setTransition('transform 0ms ease-in');
+      setMove(a);
+      setCurrPoster(b);
+    }, 400);
+    // setMove(move + 1);
+    // setCurrPoster(currPoster + 1);
+  };
   const moveWidths = [-220, -290, -320];
   const [move, setMove] = useState(0);
+  const [currPoster, setCurrPoster] = useState(3);
   const moveRef = useRef();
+
   const moveRight = () => {
-    if (move < listType.length - 1) {
+    if (currPoster < listType.length + 1) {
       setMove(move + 1);
+      setCurrPoster(currPoster + 1);
     } else {
-      setMove(0);
+      setMove(move + 1);
+      setCurrPoster(currPoster + 1);
+      replaceCarousel(-1, 2);
     }
-    // if (move < moveWidth[0].length - 1) {
-    //   // console.log('moveRight');
-    // } else if (move === moveWidth[0].length - 1) {
-    //   setMove(0);
-    // }
   };
   const moveLeft = () => {
-    setMove(move - 1);
-    // if (move != 0) {
-    //   // console.log('moveLeft');
-    // }
-    if (move > 0) {
+    if (currPoster > 2) {
       setMove(move - 1);
+      setCurrPoster(currPoster - 1);
     } else {
-      setMove(listType.length - 1);
+      setMove(move - 1);
+      setCurrPoster(currPoster - 1);
+      replaceCarousel(18, 21);
     }
   };
-  // console.log(listType);
+
+  console.log(currPoster);
+  console.log(move);
   return (
     <div className="overflow-hidden group z-40">
       <div
-        className={`w-full h-[450px] md:h-[700px] flex items-center duration-700 space-x-5 ml-[50%] mt-[-50px]`}
+        className={`w-full h-[450px] md:h-[700px] flex duration-700 items-center space-x-5 ml-[50%] mt-[-50px] border`}
         style={{
+          transition: transition,
           transform: `translateX(${
             innerWidth >= 1024
               ? moveWidths[2] * move
@@ -63,42 +86,42 @@ function MainPosters({ listType }: PropsType) {
           }px)`,
         }}
       >
-        <>
-          {listType &&
-            listType.map((e, i) => {
-              return (
-                <div key={i}>
-                  {i === move ? (
-                    <div className="relative flex w-[220px] h-[313px] md:w-[300px] md:h-[426px] lg:w-[350px] lg:h-[497px] duration-700 shrink-0 left-[-50%] rounded-lg overflow-hidden">
-                      <Image
-                        src={baseUrl + e.poster_path}
-                        sizes="100%"
-                        alt="이미지"
-                        layout="fill"
-                        objectFit="fill"
-                        className="rounded-xl"
-                        placeholder="blur"
-                        blurDataURL={baseUrl + e.poster_path}
-                      />
-                    </div>
-                  ) : (
-                    <div className="relative flex w-[200px] h-[284px] md:w-[270px] md:h-[384px] lg:w-[300px] lg:h-[426px] duration-700 shrink-0 left-[-55%] md:left-[-55.5%] lg:left-[-58.5%] rounded-lg overflow-hidden">
-                      <Image
-                        src={baseUrl + e.poster_path}
-                        sizes="100"
-                        alt="이미지"
-                        layout="fill"
-                        objectFit="fill"
-                        className="rounded-xl"
-                        placeholder="blur"
-                        blurDataURL={baseUrl + e.poster_path}
-                      />
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-        </>
+        {carousel &&
+          carousel.map((e, i) => {
+            return (
+              <div key={i}>
+                {i === currPoster ? (
+                  <div
+                    className={`relative flex w-[220px] h-[313px] md:w-[300px] md:h-[426px] lg:w-[350px] lg:h-[497px] duration-700 shrink-0 left-[-350%] md:left-[-340%] lg:left-[-325%] rounded-lg overflow-hidden`}
+                  >
+                    <Image
+                      src={baseUrl + e.poster_path}
+                      sizes="100%"
+                      alt="이미지"
+                      layout="fill"
+                      objectFit="fill"
+                      className="rounded-xl"
+                      placeholder="blur"
+                      blurDataURL={baseUrl + e.poster_path}
+                    />
+                  </div>
+                ) : (
+                  <div className="relative flex w-[200px] h-[284px] md:w-[270px] md:h-[384px] lg:w-[300px] lg:h-[426px] duration-700 shrink-0 left-[-385%] md:left-[-378%] lg:left-[-379.5%] rounded-lg overflow-hidden">
+                    <Image
+                      src={baseUrl + e.poster_path}
+                      sizes="100"
+                      alt="이미지"
+                      layout="fill"
+                      objectFit="fill"
+                      className="rounded-xl"
+                      placeholder="blur"
+                      blurDataURL={baseUrl + e.poster_path}
+                    />
+                  </div>
+                )}
+              </div>
+            );
+          })}
       </div>
       <div className="top-[15%] md:top-[20%] absolute invisible group-hover:visible flex justify-between items-center w-full">
         <div
