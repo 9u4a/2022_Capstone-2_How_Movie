8eb287378e53a541f515b595b60c834704f00c13
UBamtol <yij0325@gmail.com>
2022-10-27 23:56:58 +0900

    [ Web ] Add tailwindcss/line-clamp

--- HowMovie_Web/components/Comment.tsx
+++ HowMovie_Web/components/Comment.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StarIcon } from '@heroicons/react/24/solid';
+// interface sessionType {
+//   data: {
+//     expries: string;
+//     user: {
+//       email: string;
+//       iamge: string;
+//       name: string;
+//     };
+//   };
+//   status: string;
+// }
+function Comment({ testProps }: any) {
+  console.log(testProps);
+  // useEffect(() => {}, [testProps]);
 
-function Comment() {
   return (
-    <div className="h-[140px] p-5 bg-slate-800 mb-3 rounded-md">
-      <div className="flex justify-between">
-        <div className="text-sm mb-1">영화조아</div>
-        <div className="space-x-3 text-sm">
-          <button>수정</button>
-          <button>삭제</button>
+    <>
+      {testProps === undefined ? (
+        <div className="h-[140px] p-5 bg-slate-800 mb-3">
+          <div className="text-sm mb-1">대림이</div>
+          <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-5">
+            <StarIcon width={15} height={15} /> <p className="ml-2">3.6</p>
+          </div>
+          <p>연기가 조금 아쉬웠요..</p>
         </div>
-      </div>
-      <div className="flex items-center justify-center border w-[55px] h-[25px] rounded-md mb-5">
-        <StarIcon width={13} height={13} className="text-[#d70000]" />{' '}
-        <p className="ml-2 text-sm">5.0</p>
-      </div>
-      <p>진짜 재밌어요</p>
-    </div>
+      ) : (
+        <div className="h-[140px] p-5 bg-slate-800 mb-3">
+          <div className="text-sm mb-1">
+            {testProps?.session.data.user.name}
+          </div>
+          <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-5">
+            <StarIcon width={15} height={15} />{' '}
+            <p className="ml-2">{testProps?.starClick.toFixed(1)}</p>
+          </div>
+          <p>{testProps?.userComment}</p>
+        </div>
+      )}
+    </>
   );
 }
 
