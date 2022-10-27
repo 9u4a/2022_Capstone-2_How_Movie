10e0f76d3d5ebd44a0075129b54a5083842a259b
Mallang-Mallang <gudwns1458@naver.com>
2022-10-27 10:44:57 +0900

    [ Web ] Fix Comment Box Css

--- HowMovie_Web/components/Comment.tsx
+++ HowMovie_Web/components/Comment.tsx
@@ -4,10 +4,16 @@ import { StarIcon } from '@heroicons/react/24/solid';
 function Comment() {
   return (
     <div className="h-[140px] p-5 bg-slate-800 mb-3 rounded-md">
-      <div className="text-sm mb-1">영화조아</div>
-      <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-5">
-        <StarIcon width={15} height={15} className="text-[#d70000]" />{' '}
-        <p className="ml-2">5.0</p>
+      <div className="flex justify-between">
+        <div className="text-sm mb-1">영화조아</div>
+        <div className="space-x-3 text-sm">
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center border w-[55px] h-[25px] rounded-md mb-5">
+        <StarIcon width={13} height={13} className="text-[#d70000]" />{' '}
+        <p className="ml-2 text-sm">5.0</p>
       </div>
       <p>진짜 재밌어요</p>
     </div>
