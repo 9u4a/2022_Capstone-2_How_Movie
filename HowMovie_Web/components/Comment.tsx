aaf0b4a2dbcafc3e36953a548530d8e7d98c884a
UBamtol <yij0325@gmail.com>
2022-10-31 22:52:34 +0900

    [ Web ] Fix Comment props, Add delete button

--- HowMovie_Web/components/Comment.tsx
+++ HowMovie_Web/components/Comment.tsx
@@ -1,42 +1,30 @@
 import React, { useEffect } from 'react';
 import { StarIcon } from '@heroicons/react/24/solid';
-// interface sessionType {
-//   data: {
-//     expries: string;
-//     user: {
-//       email: string;
-//       iamge: string;
-//       name: string;
-//     };
-//   };
-//   status: string;
-// }
-function Comment({ testProps }: any) {
-  console.log(testProps);
+
+function Comment({ commentInfo }: any) {
+  console.log(commentInfo);
+
   // useEffect(() => {}, [testProps]);
 
   return (
     <>
-      {testProps === undefined ? (
-        <div className="h-[140px] p-5 bg-slate-800 mb-3">
-          <div className="text-sm mb-1">대림이</div>
-          <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-5">
-            <StarIcon width={15} height={15} /> <p className="ml-2">3.6</p>
+      {commentInfo.result.map((e: any, i: number) => {
+        return (
+          <div className="h-[140px] p-5 bg-slate-800 mb-3" key={i}>
+            <div className="flex text-sm mb-1 justify-between">
+              <div>{e.user_id}</div>
+              <div className="text-slate-500 hover:text-white active:text-slate-400 hover:cursor-pointer">
+                삭제
+              </div>
+            </div>
+            <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-3">
+              <StarIcon width={15} height={15} />
+              <p className="ml-2">{e.vote.toFixed(1)}</p>
+            </div>
+            <div>{e.comment}</div>
           </div>
-          <p>연기가 조금 아쉬웠요..</p>
-        </div>
-      ) : (
-        <div className="h-[140px] p-5 bg-slate-800 mb-3">
-          <div className="text-sm mb-1">
-            {testProps?.session.data.user.name}
-          </div>
-          <div className="flex items-center justify-center mr-5 border w-[60px] rounded-md mb-5">
-            <StarIcon width={15} height={15} />{' '}
-            <p className="ml-2">{testProps?.starClick.toFixed(1)}</p>
-          </div>
-          <p>{testProps?.userComment}</p>
-        </div>
-      )}
+        );
+      })}
     </>
   );
 }
