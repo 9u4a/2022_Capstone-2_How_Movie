cb67efdcbd6f3518adebf9ed433d55485fa02e82
9u4a <99junsung@gmail.com>
2022-11-01 11:30:24 +0900

    [ API ] Update CommentView PUT, DELETE

--- HowMovie_API/API/models.py
+++ HowMovie_API/API/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Comment(models.Model):
-    user_id = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
     movie_id = models.CharField(max_length=50)
     comment = models.CharField(max_length=1000)
     vote = models.FloatField(max_length=5)
+    email = models.EmailField(max_length=50)
 
     class Meta:
         db_table = 'API_Comment'
