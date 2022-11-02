c131d7250215f614f6b59c1869692f7a353d3be0
9u4a <99junsung@gmail.com>
2022-11-02 13:22:47 +0900

    [ API ] MyCommentView Update PUT, DELETE

--- HowMovie_API/API/Views/MyCommentView.py
+++ HowMovie_API/API/Views/MyCommentView.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import FieldDoesNotExist
 from django.db.models import Q
 from django.http import JsonResponse
 from rest_framework import status
@@ -10,21 +9,27 @@ from API.models import Comment
 
 class MyCommentView(APIView):
     def get(self, request):
-        email = request.GET.get('email')
-        comment_data = list(Comment.objects.filter(email=email).values())
-        print(comment_data)
-        response = {
-            'success': True,
-            'result': comment_data
-        }
-        return JsonResponse(response)
+        try:
+            email = request.GET.get('email')
+            comment_data = list(Comment.objects.filter(email=email).values())
+            print(comment_data)
+            response = {
+                'success': True,
+                'result': comment_data
+            }
+            return JsonResponse(response, status=status.HTTP_200_OK)
+        except Comment.DoesNotExist:
+            response = {
+                'success': False,
+                'err': 'Comment Does Not Exist'
+            }
+        return JsonResponse(response, status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request):
         data = JSONParser().parse(request)
-        email = request.GET.get('email')
         comment_data = Comment.objects.filter(
             Q(id=data['id']) &
-            Q(email=email)
+            Q(email=data['email'])
         ).first()
         comment_serializer = CommentSerializer(comment_data, data=data)
         if comment_serializer.is_valid():
@@ -33,24 +38,35 @@ class MyCommentView(APIView):
                 'success': True,
                 'user_name': data['user_name']
             }
-            return JsonResponse(response)
+            return JsonResponse(response, status=status.HTTP_201_CREATED)
+        else:
+            response = {
+                'success': False,
+                'err': comment_serializer.errors
+            }
+            return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         data = JSONParser().parse(request)
-        email = request.GET.get('email')
         try:
             comment_data = Comment.objects.filter(
                 Q(id=data['id']) &
-                Q(email=email)
+                Q(email=data['email'])
             ).first()
             comment_data.delete()
             response = {
                 'success': True
             }
-            return JsonResponse(response)
-        except FieldDoesNotExist:
+            return JsonResponse(response, status=status.HTTP_200_OK)
+        except AttributeError:
             response = {
                 'success': False,
-                'error': 'FieldDoesNotExist'
+                'error': 'Comment Does Not Exist'
+            }
+            return JsonResponse(response, status=status.HTTP_404_NOT_FOUND)
+        except:
+            response = {
+                'success': False,
+                'error': 'Comment Dose Not Exist'
             }
             return JsonResponse(response, status=status.HTTP_404_NOT_FOUND)
