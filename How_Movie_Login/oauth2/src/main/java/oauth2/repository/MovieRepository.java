3f0fcdd96a07c42437996f3b682991f952fbc77d
Tasty-Programmer <kimhunsup159@gmail.com>
2022-11-02 21:06:26 +0900

    [ Login ]  Remove controller , entity , repository , service

--- How_Movie_Login/oauth2/src/main/java/oauth2/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package oauth2.repository;
-
-import oauth2.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
